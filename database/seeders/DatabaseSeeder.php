f5aee29d758cd482b5d4f3168266dce1462bc7bd
blcoussama <belcadioussama.eng@gmail.com>
2025-07-01 16:20:14 +0100

    fixed seeding issue

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('password123'),
         ]);
 
-        Feature::factory()->create([
+        Feature::create([
             'route_name' => 'feature1.index',
             'image' => 'https://images.unsplash.com/photo-1662947190722-5d272f82a526?q=80&w=2728&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
             'name' => 'Calculate Sum',
@@ -30,7 +30,7 @@ class DatabaseSeeder extends Seeder
             'required_credits' => 1,
             'is_active' => true,
         ]);
-        Feature::factory()->create([
+        Feature::create([
             'route_name' => 'feature2.index',
             'image' => 'https://i0.wp.com/blog.openapihub.com/wp-content/uploads/2022/08/Slide1-1.jpg?resize=1200%2C675&ssl=1',
             'name' => 'Calculate Difference',
@@ -39,17 +39,17 @@ class DatabaseSeeder extends Seeder
             'is_active' => true,
         ]);
 
-        Package::factory()->create([
+        Package::create([
             'name' => 'Basic Package',
             'price' => 9.99,
             'credits' => 20,
         ]);
-        Package::factory()->create([
+        Package::create([
             'name' => 'Silver Package',
             'price' => 99.99,
             'credits' => 100,
         ]);
-        Package::factory()->create([
+        Package::create([
             'name' => 'GVolden Package',
             'price' => 199.99,
             'credits' => 500,
